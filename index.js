09a40a1757402bec1f8032123c8b3113c3cbd386
Pardo Jonathan <yonipardo4@gmail.com>
2022-08-13 13:56:48 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,119 @@
+let input = document.querySelector("input");
+let encriptar = document.getElementById("encriptar");
+let desencriptar = document.getElementById("desencriptar");
+let copiar = document.getElementById("btn-copy");
+let answer = document.getElementById("answer");
+let aleatorio = document.getElementById("aleatorio");
+
+
+
+// FUNCION PARA ENCRIPTAR EL MENSAJE
+function encriptarFn() {
+  let frase = input.value;
+  let fraseEncriptadaarr = [];
+
+  for (let i = 0; i < frase.length; i++) {
+    let letra;
+    switch (frase[i]) {
+      case "a":
+        letra = "ai";
+        break;
+      case "e":
+        letra = "enter";
+        break;
+      case "i":
+        letra = "imes";
+        break;
+      case "o":
+        letra = "ober";
+        break;
+      case "u":
+        letra = "ufat";
+        break;
+      default:
+        letra = frase[i];
+        break;
+    }
+    fraseEncriptadaarr.push(letra);
+  }
+  frase = fraseEncriptadaarr.join("");
+  answer.innerHTML = frase;
+  copiar.style.display = "block";
+
+  document.getElementById("message-card").style.display = "none";
+  document.getElementById("message-answer").style.display = "flex";
+
+  return fraseEncriptadaarr;
+}
+
+// FUNCION PARA DESENCRIPTAR EL MENSAJE
+function desencriptarFn() {
+  let frase = input.value.toLowerCase();
+
+  let contador = 0;
+  while (contador < frase.length) {
+    if (frase.includes("ai")) {
+      frase = frase.replace("ai", "a");
+    }
+    if (frase.includes("enter")) {
+      frase = frase.replace("enter", "e");
+    }
+    if (frase.includes("imes")) {
+      frase = frase.replace("imes", "i");
+    }
+    if (frase.includes("ober")) {
+      frase = frase.replace("ober", "o");
+    }
+    if (frase.includes("ufat")) {
+      frase = frase.replace("ufat", "u");
+    }
+    contador++;
+  }
+  // console.log(frase);
+  answer.innerHTML = frase;
+  copiar.style.display = "block";
+
+  document.getElementById("message-card").style.display = "none";
+  document.getElementById("message-answer").style.display = "flex";
+}
+
+
+
+function validarCampo(campo) {
+  let regEx = /\b[a-z]/g;
+
+  if (regEx.test(campo) == true) {
+    input.classList.remove("error");
+    input.classList.add("correcto");
+    document.querySelector(".text-error").style.display = "none";
+  } else {
+    input.classList.add("error");
+    input.classList.remove("correcto");
+    document.querySelector(".text-error").style.display = "block";
+  }
+}
+
+encriptar.addEventListener("click", (event) => {
+  event.preventDefault();
+  validarCampo(input.value);
+});
+
+desencriptar.addEventListener("click", (event) => {
+  event.preventDefault();
+  validarCampo(input.value);
+});
+
+encriptar.onclick = encriptarFn;
+
+desencriptar.onclick = desencriptarFn;
+
+
+
+// FUNCION COPIAR EL MENSAJE
+function copiarFn() {
+  let content = document.getElementById("answer").innerText;
+  content.select;
+  navigator.clipboard.writeText(content);
+}
+
+copiar.onclick = copiarFn;
